9dcbfe598cc03b2f0f1ce1d651166107ae98b5bf
crypticsurfer <cb.connor.bishop@gmail.com>
2023-12-03 14:57:13 -0700

    added routes and structure

--- professional-react-portfolio/src/main.jsx
+++ professional-react-portfolio/src/main.jsx
@@ -3,8 +3,45 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Portfolio from './pages/portfolio';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Resume from './pages/resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: '',
+        element: <About />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        index: true,
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
