45786db659c6d6eada1cc0053901a8f9101cd8c2
crypticsurfer <cb.connor.bishop@gmail.com>
2023-12-03 16:48:42 -0700

    added contact and css

--- professional-react-portfolio/src/pages/contact.jsx
+++ professional-react-portfolio/src/pages/contact.jsx
@@ -1,5 +1,27 @@
+import "./Contact.css";
+
 export default function Contact() {
     return (
-        <div></div>
+        <div className="contact">
+            <div className="content-box">
+                <h1>Contact</h1>
+                <div className="width-box">
+                    <h2>Name:</h2>
+                    <input></input>
+                </div>
+
+                <div className="width-box">
+                    <h2>Email:</h2>
+                    <input></input>
+                </div>
+
+                <div className="message">
+                    <h2>Message:</h2>
+                    <textarea></textarea>
+                </div>
+
+                <button>Submit</button>
+            </div>
+        </div>
     )
 }
